dd36206e0518251a4b9bf98a9124b9c6cc27d1e5
eudora <eudora@live.com>
2014-03-29 20:55:57 +0000

    Create gfun.cpp

---
+++ gfun.cpp
@@ -0,0 +1,18 @@
+/*  Belinda Li, Crystina, Kat
+    gfun.cpp - Group Function Project
+    3/29/14
+    Make some things do some other things
+*/    
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+
+{
+
+
+return 0;
+}
